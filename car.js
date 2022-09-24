d93e8d765164bf3668d962c955926b35bce9cf19
meeroTheo <meerbalochandhammy12@gmail.com>
2022-09-24 19:54:24 -0400

    Draw the car as a rectangle

---
+++ car.js
@@ -0,0 +1,24 @@
+class Car {
+    //car object constructor
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+    }
+    //method that takes context as param
+    draw(ctx) {
+        ctx.beginPath();
+        //draw the car as a rectangle
+        ctx.rect(
+            //it starts at x,y
+            //the x is the center of the car
+            this.x - this.width / 2,
+            this.y - this.height / 2,
+            this.width,
+            this.height
+        );
+        ctx.fill();
+    }
+}
