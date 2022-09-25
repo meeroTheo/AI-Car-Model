edf2ee19f9cbfbdd81c7a7fb87187767b33de052
meeroTheo <meerbalochandhammy12@gmail.com>
2022-09-24 20:01:36 -0400

    Add a controls class

--- car.js
+++ car.js
@@ -5,15 +5,14 @@ class Car {
         this.y = y;
         this.width = width;
         this.height = height;
-
     }
     //method that takes context as param
     draw(ctx) {
         ctx.beginPath();
         //draw the car as a rectangle
+        //it starts at x,y
+        //the x is the center of the car
         ctx.rect(
-            //it starts at x,y
-            //the x is the center of the car
             this.x - this.width / 2,
             this.y - this.height / 2,
             this.width,
