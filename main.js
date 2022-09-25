29e2d4680737ca0a3935b9faa271f0d127a989d9
meeroTheo <meerbalochandhammy12@gmail.com>
2022-09-24 20:18:57 -0400

    Add car movement animation to main.js

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const canvas = document.getElementById("myCanvas");
-canvas.height = window.innerHeight;
 canvas.width = 200;
 
 //Reference to canvas context that contains
 //all the drawing methods
 const ctx = canvas.getContext("2d");
 const car = new Car(100, 100, 30, 50);
-car.draw(ctx);
 
+//start to animate the car
+animate();
+
+function animate() {
+    car.update();
+    canvas.height = window.innerHeight;
+    car.draw(ctx);
+    //CONSISTENTLY CALLS animation 
+    //gives illusion of movement
+    requestAnimationFrame(animate);
+
+}
