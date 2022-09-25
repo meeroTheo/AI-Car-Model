2bf0d9d18b231b931a8d9b7817ff19f10e3f2c44
meeroTheo <meerbalochandhammy12@gmail.com>
2022-09-24 20:25:08 -0400

    Add friction, acceleration, and max speed to the car

--- car.js
+++ car.js
@@ -6,16 +6,38 @@ class Car {
         this.width = width;
         this.height = height;
 
+        this.speed = 0;
+        this.acceleration = 0.2;
+
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+
         this.controls = new Controls()
     }
     //check if a control is pressed, and move
     update() {
         if (this.controls.forward) {
-            this.y -= 2; //y increases as you go down
+            this.speed += this.acceleration; //y increases as you go down
         }
         if (this.controls.reverse) {
-            this.y += 2;
+            this.speed -= this.acceleration;
         }
+
+        if (this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        }
+        //-this.maxSpeed indicates car moving backwards
+        if (this.speed < -this.maxSpeed / 2) {
+            this.speed = -this.maxSpeed / 2;
+        }
+        //decrease speed due to friction
+        if (this.speed > 0) {
+            this.speed -= this.friction;
+        }
+        if (this.speed < 0) {
+            this.speed += this.friction;
+        }
+        this.y -= this.speed;
     }
     //method that takes context as param
     draw(ctx) {
