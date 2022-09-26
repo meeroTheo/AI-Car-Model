a976f4d155abe714f8cbb9c4c3b2b7488e3ebd42
meeroTheo <meerbalochandhammy12@gmail.com>
2022-09-26 14:19:49 -0400

    Draw a line on the left and right of the road

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ canvas.width = 200;
 //Reference to canvas context that contains
 //all the drawing methods
 const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width / 2, canvas.width);
 const car = new Car(100, 100, 30, 50);
 
 //start to animate the car
@@ -12,6 +13,7 @@ animate();
 function animate() {
     car.update();
     canvas.height = window.innerHeight;
+    road.draw(ctx);
     car.draw(ctx);
     //CONSISTENTLY CALLS animation 
     //gives illusion of movement
--- road.js
+++ road.js
@@ -8,7 +8,6 @@ class Road {
         this.right = x + width / 2;
 
         const infinity = 1000000;
-
         this.top = -infinity;
         this.bottom = infinity;
 
@@ -18,15 +17,15 @@ class Road {
         ctx.lineWidth = 5;
         ctx.strokeStyle = 'white';
 
-        //make a vertical line
+        //make a vertical line on the left side of the screen
         ctx.beginPath();
         ctx.moveTo(this.left, this.top);
         ctx.lineTo(this.left, this.bottom);
-        this.stroke();
+        ctx.stroke();
 
         ctx.beginPath();
         ctx.moveTo(this.right, this.top);
         ctx.lineTo(this.right, this.bottom);
-        this.stroke();
+        ctx.stroke();
     }
 }
